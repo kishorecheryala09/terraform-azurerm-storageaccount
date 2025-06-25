f55af40aff3040d45bf2f003269b6cff777017ca
Kishore Cheryala <kishore.cheryala@avanade.com>
2025-06-25 12:00:54 +0100

    Initial Checkin

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  description = "The name of the resource group where the resources will be created."
+  type        = string
+}
+variable "location" {
+  description = "The Azure region where the resources will be created."
+  type        = string
+}
+variable "storage_account_name" {
+  description = "The name of the storage account to be created."
+  type        = string
+}
+variable "environment" {
+  description = "The environment for which the resources are being created (e.g., production, staging, development)."
+  type        = string
+}
